bfd9138da8b7751b9cfc7762c31cfcb3a2f92f5b
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-05-26 19:37:52 +0530

    Update Stack-NextGreaterElement.cpp

--- Stack-NextGreaterElement.cpp
+++ Stack-NextGreaterElement.cpp
@@ -7,29 +7,29 @@ using namespace std;
 void NGE(int *arr, int n)
 {
 
-    stack<int> s;
-    int nge[n];
-      s.push(n-1);
-      s.push(n-1);
-      nge[n-1]=-1;
-
-    for (int i =n-2; i >= 0; i--)
-    {
-       while(s.size()>0 && arr[i]>=arr[s.top()]){
-         s.pop();
-       }
-
-       if(s.size()==0){
-           nge[i]=-1;
-
-       }
-       else{
-           nge[i]=s.top();
-             nge[i]=arr[s.top()];
-       }
-       s.push(i);
-
-    }
+      
+        stack<long long>st;
+        st.push(n-1);
+        vector<long long>nge(n,-1);
+        
+        
+        for(int i=n-2;i>=0;i--){
+            
+            while(st.size()>0 and arr[i]>arr[st.top()]){
+                st.pop();
+            }
+            
+            if(st.size()==0){
+                nge[i]=-1;
+            }
+            else{
+                nge[i]=arr[st.top()];
+            }
+            
+            st.push(i);
+        }
+        
+        return nge;
 
     for(int i=0;i<n;i++){
         cout<<nge[i]<<" ";
