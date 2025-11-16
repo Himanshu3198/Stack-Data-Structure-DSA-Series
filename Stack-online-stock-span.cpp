55bf35eb53ef25a35b5fd066da8286507afd51ad
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2025-11-16 20:12:29 +0530

    Implement StockSpanner class in Java and Python

--- Stack-online-stock-span.cpp
+++ Stack-online-stock-span.cpp
@@ -34,3 +34,63 @@ public:
  * StockSpanner* obj = new StockSpanner();
  * int param_1 = obj->next(price);
  */
+
+// python
+
+class StockSpanner:
+
+    def __init__(self):
+        self.st = []
+
+    def next(self, price: int) -> int:
+        curr_count = 1
+        while self.st and self.st[-1][0] <= price:
+            curr_count +=self.st[-1][1]
+            self.st.pop()
+        self.st.append((price,curr_count))
+        return curr_count
+            
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+    // java code
+
+    class StockSpanner {
+   
+    private Stack<Pair> st;
+    public StockSpanner() {
+         this.st = new Stack<>();
+
+    }
+    
+    public int next(int price) {
+        
+        int currCount = 1;
+
+        while(!st.isEmpty() && st.peek().price <= price){
+            currCount += st.pop().count;
+        }
+        st.push(new Pair(price,currCount));
+
+        return currCount;
+    }
+}
+
+class Pair{
+    int price;
+    int count;
+
+    public Pair(int price,int count){
+        this.price = price;
+        this.count = count;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
